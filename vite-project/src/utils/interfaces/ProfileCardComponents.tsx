813b09629f08944be667326b0903e87922a34bd9
sylviavitoria <sylviavitoriasilva@gmail.com>
2024-12-12 16:59:39 -0300

    feat: Foi criado um componente card-Profile

---
+++ vite-project/src/utils/interfaces/ProfileCardComponents.tsx
@@ -0,0 +1,10 @@
+export interface ProfileCardProps {
+    name: string;
+    especifico: string;
+    dataNacimento: string;
+    cidade: string;
+    estado: string;
+    profile: string;
+    profissao: string;
+  }
+  
