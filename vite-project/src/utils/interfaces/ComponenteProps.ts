63d1ed738d150676ffc4c9df2109cd996754ba64
Danny Marques <daninhacmmarques@gmail.com>
2024-12-14 11:22:46 -0300

    update: foi refatorado novamente o app.tsx para uma melhor estruturação e modificação do arquivo layout e criação de uma unica interface de componente

--- vite-project/src/utils/interfaces/ComponenteProps.ts
+++ vite-project/src/utils/interfaces/ComponenteProps.ts
@@ -7,9 +7,21 @@ export  interface ModalProps {
     size: unknown;
   }
 
-
   export type BotaoProps = {
     tipo: "criar" | "confirmar" | "rejeitar" | "editar";
     onClick: () => void;
     buttonType?: "button" | "submit";
   };
+
+  export interface ProfileCardProps {
+    name: string;
+    especifico: string;
+    dataNacimento: string;
+    cidade: string;
+    estado: string;
+    profile: string;
+    profissao: string;
+  }
+ export  interface ChildrenProps {
+    children: React.ReactNode;
+  }
